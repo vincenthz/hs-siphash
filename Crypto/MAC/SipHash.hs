49f7703e8dc90fa33f1fa881b931137674cb99cc
Vincent Hanquez <vincent@snarc.org>
2012-09-30 15:48:32 +0100

    Increase performance as initial code is slow.
    Use more unsafe operations (which are all safe in this context):
    
    - prevent unnecessary checks and boxing.
    - directly peek in the bytestring instead of using cereal.
    - add some constants propagation using more inlining.
    - misc modifications so that ghc yield better code.
    
    For really small hash, the performance increases by 25x to 33x.
    For large input (~1mb), the performance increases up to 80x.

--- Crypto/MAC/SipHash.hs
+++ Crypto/MAC/SipHash.hs
@@ -8,6 +8,7 @@
 -- provide the SipHash algorithm.
 -- reference: <http://131002.net/siphash/siphash.pdf>
 --
+{-# LANGUAGE BangPatterns #-}
 module Crypto.MAC.SipHash
     ( SipKey(..)
     , SipHash(..)
@@ -17,11 +18,15 @@ module Crypto.MAC.SipHash
 
 import Data.Word
 import Data.Bits
-import Data.List (foldl')
-import Data.Serialize.Get
 import Data.ByteString (ByteString)
+import Data.ByteString.Internal
 import qualified Data.ByteString as B
-import Control.Monad (foldM,replicateM)
+import Control.Monad
+
+import Foreign.ForeignPtr
+import Foreign.Ptr
+import Foreign.Storable
+import System.Endian (fromLE64)
 
 -- | SigHash Key
 data SipKey = SipKey {-# UNPACK #-} !Word64 {-# UNPACK #-} !Word64
@@ -38,49 +43,117 @@ hash = hashWith 2 4
 
 -- | same as 'hash', except also specifies the number of sipround iterations for compression and digest.
 hashWith :: Int -> Int -> SipKey -> ByteString -> SipHash
-hashWith c d key b = either error (finish d) $ runGet runHash b
-    where len = B.length b
-          (nb,last) = len `divMod` 8
+hashWith c d key (PS ps s fl) = inlinePerformIO $ withForeignPtr ps (\ptr -> runHash (initSip key) (ptr `plusPtr` s) fl)
+    where runHash !st !ptr l
+                | l > 7     = fromLE64 `fmap` peek (castPtr ptr) >>= \v -> runHash (process st v) (ptr `plusPtr` 8) (l-8)
+                | otherwise = do
+                    let !lengthBlock = (fromIntegral fl `mod` 256) `unsafeShiftL` 56
+                    (finish . process st) `fmap` case l of
+                        0 -> do return lengthBlock
+                        1 -> do v0 <- peekByteOff ptr 0
+                                return (lengthBlock .|. to64 v0)
+                        2 -> do (v0,v1) <- liftM2 (,) (peekByteOff ptr 0) (peekByteOff ptr 1)
+                                return (lengthBlock
+                                        .|. (to64 v1 `unsafeShiftL` 8)
+                                        .|. to64 v0)
+                        3 -> do (v0,v1,v2) <- liftM3 (,,) (peekByteOff ptr 0) (peekByteOff ptr 1) (peekByteOff ptr 2)
+                                return (    lengthBlock
+                                        .|. (to64 v2 `unsafeShiftL` 16)
+                                        .|. (to64 v1 `unsafeShiftL` 8)
+                                        .|. to64 v0)
+                        4 -> do (v0,v1,v2,v3) <- liftM4 (,,,) (peekByteOff ptr 0) (peekByteOff ptr 1) (peekByteOff ptr 2)
+                                                              (peekByteOff ptr 3)
+                                return (    lengthBlock
+                                        .|. (to64 v3 `unsafeShiftL` 24)
+                                        .|. (to64 v2 `unsafeShiftL` 16)
+                                        .|. (to64 v1 `unsafeShiftL` 8)
+                                        .|. to64 v0)
+                        5 -> do (v0,v1,v2,v3,v4) <- liftM5 (,,,,) (peekByteOff ptr 0) (peekByteOff ptr 1) (peekByteOff ptr 2)
+                                                                  (peekByteOff ptr 3) (peekByteOff ptr 4)
+                                return (    lengthBlock
+                                        .|. (to64 v4 `unsafeShiftL` 32)
+                                        .|. (to64 v3 `unsafeShiftL` 24)
+                                        .|. (to64 v2 `unsafeShiftL` 16)
+                                        .|. (to64 v1 `unsafeShiftL` 8)
+                                        .|. to64 v0)
+                        6 -> do v0 <- peekByteOff ptr 0
+                                v1 <- peekByteOff ptr 1
+                                v2 <- peekByteOff ptr 2
+                                v3 <- peekByteOff ptr 3
+                                v4 <- peekByteOff ptr 4
+                                v5 <- peekByteOff ptr 5
+                                return (    lengthBlock
+                                        .|. (to64 v5 `unsafeShiftL` 40)
+                                        .|. (to64 v4 `unsafeShiftL` 32)
+                                        .|. (to64 v3 `unsafeShiftL` 24)
+                                        .|. (to64 v2 `unsafeShiftL` 16)
+                                        .|. (to64 v1 `unsafeShiftL` 8)
+                                        .|. to64 v0)
+                        7 -> do v0 <- peekByteOff ptr 0
+                                v1 <- peekByteOff ptr 1
+                                v2 <- peekByteOff ptr 2
+                                v3 <- peekByteOff ptr 3
+                                v4 <- peekByteOff ptr 4
+                                v5 <- peekByteOff ptr 5
+                                v6 <- peekByteOff ptr 6
+                                return (    lengthBlock
+                                        .|. (to64 v6 `unsafeShiftL` 48)
+                                        .|. (to64 v5 `unsafeShiftL` 40)
+                                        .|. (to64 v4 `unsafeShiftL` 32)
+                                        .|. (to64 v3 `unsafeShiftL` 24)
+                                        .|. (to64 v2 `unsafeShiftL` 16)
+                                        .|. (to64 v1 `unsafeShiftL` 8)
+                                        .|. to64 v0)
 
-          runHash = foldM (const . getBlock) (initSip key)[0..(nb-1)] >>= lastBlock
-          getBlock st = process c st `fmap` getWord64le
-          lastBlock st = do
-              let lengthBlock = fromIntegral (len `mod` 256) `shiftL` 56
-              z <- (fst . foldl shiftAndAdd (0,0)) `fmap` replicateM last getWord8
-              return $ process c st (lengthBlock .|. z)
+          {-# INLINE to64 #-}
+          to64 :: Word8 -> Word64
+          to64 = fromIntegral
 
-          shiftAndAdd :: (Word64,Int) -> Word8 -> (Word64,Int)
-          shiftAndAdd (acc,pos) v = (acc .|. ((fromIntegral v) `shiftL` pos), pos+8)
+          {-# INLINE process #-}
+          process istate m = newState
+              where newState = postInject $! runRoundsCompression $! preInject istate
+                    preInject  (InternalState v0 v1 v2 v3) = InternalState v0 v1 v2 (v3 `xor` m)
+                    postInject (InternalState v0 v1 v2 v3) = InternalState (v0 `xor` m) v1 v2 v3
 
-initSip (SipKey k0 k1) = InternalState (k0 `xor` 0x736f6d6570736575)
-                                       (k1 `xor` 0x646f72616e646f6d)
-                                       (k0 `xor` 0x6c7967656e657261)
-                                       (k1 `xor` 0x7465646279746573)
+          {-# INLINE finish #-}
+          finish istate = getDigest $! runRoundsDigest $! preInject istate
+              where getDigest (InternalState v0 v1 v2 v3) = SipHash (v0 `xor` v1 `xor` v2 `xor` v3)
+                    preInject (InternalState v0 v1 v2 v3) = InternalState v0 v1 (v2 `xor` 0xff) v3
 
-doRound (InternalState v0 v1 v2 v3) = do
-    let v0'    = v0 + v1
-        v2'    = v2 + v3
-        v1'    = v1 `rotateL` 13
-        v3'    = v3 `rotateL` 16
-        v1''   = v1' `xor` v0'
-        v3''   = v3' `xor` v2'
-        v0''   = v0' `rotateL` 32
-        v2''   = v2' + v1''
-        v0'''  = v0'' + v3''
-        v1'''  = v1'' `rotateL` 17
-        v3'''  = v3'' `rotateL` 21
-        v1'''' = v1''' `xor` v2''
-        v3'''' = v3''' `xor` v0'''
-        v2'''  = v2'' `rotateL` 32
-     in InternalState v0''' v1'''' v2''' v3''''
+          {-# INLINE doRound #-}
+          doRound (InternalState v0 v1 v2 v3) =
+              let !v0'    = v0 + v1
+                  !v2'    = v2 + v3
+                  !v1'    = v1 `rotateL` 13
+                  !v3'    = v3 `rotateL` 16
+                  !v1''   = v1' `xor` v0'
+                  !v3''   = v3' `xor` v2'
+                  !v0''   = v0' `rotateL` 32
+                  !v2''   = v2' + v1''
+                  !v0'''  = v0'' + v3''
+                  !v1'''  = v1'' `rotateL` 17
+                  !v3'''  = v3'' `rotateL` 21
+                  !v1'''' = v1''' `xor` v2''
+                  !v3'''' = v3''' `xor` v0'''
+                  !v2'''  = v2'' `rotateL` 32
+               in InternalState v0''' v1'''' v2''' v3''''
 
-runRounds n st = foldl' (const . doRound) st [0..(n-1)]
+          {-# INLINE runRoundsCompression #-}
+          runRoundsCompression st
+            | c == 2    = doRound $! doRound st
+            | otherwise = loopRounds c st
 
-process c istate m = newState
-    where newState = postInject $ runRounds c $ preInject $ istate
-          preInject  (InternalState v0 v1 v2 v3) = InternalState v0 v1 v2 (v3 `xor` m)
-          postInject (InternalState v0 v1 v2 v3) = InternalState (v0 `xor` m) v1 v2 v3
+          {-# INLINE runRoundsDigest #-}
+          runRoundsDigest st
+            | d == 4    = doRound $! doRound $! doRound $! doRound st
+            | otherwise = loopRounds d st
 
-finish d istate = getDigest $ runRounds d $ preInject istate
-    where getDigest (InternalState v0 v1 v2 v3) = SipHash (v0 `xor` v1 `xor` v2 `xor` v3)
-          preInject (InternalState v0 v1 v2 v3) = InternalState v0 v1 (v2 `xor` 0xff) v3
+          {-# INLINE loopRounds #-}
+          loopRounds 1 !v = doRound v
+          loopRounds n !v = loopRounds (n-1) (doRound v)
+
+          {-# INLINE initSip #-}
+          initSip (SipKey k0 k1) = InternalState (k0 `xor` 0x736f6d6570736575)
+                                                 (k1 `xor` 0x646f72616e646f6d)
+                                                 (k0 `xor` 0x6c7967656e657261)
+                                                 (k1 `xor` 0x7465646279746573)
